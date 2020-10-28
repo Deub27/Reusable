3badc9c424899c4544cb34c5d2c2484b7567ef22
Sebastien DEBERDT <sebastien.deberdt@gmail.com>
2020-10-28 14:37:40 +0100

    Update

--- Package.swift
+++ Package.swift
@@ -1,9 +1,9 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.0
 import PackageDescription
 
 let package = Package(
     name: "Reusable",
-    platforms: [.iOS(.v9), .tvOS(.v9)],
+    platforms: [.iOS(.v9)],
     products: [
         .library(name: "Reusable", targets: ["Reusable"])
     ],
